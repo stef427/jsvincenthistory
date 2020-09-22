39e0f5eea3cc0704cf3abd4314faf1f8266c1813
stef427 <stef_427@hotmail.com>
2020-09-22 16:36:23 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function center() {
+    document.getElementById("clique").style.backgroundColor = "red";
+    document.getElementById("clique").style.display = "flex";
+    document.getElementById("clique").style.justifyContent = "space-around";
+    document.getElementById("clique").style.listStyle = "none";
+}
